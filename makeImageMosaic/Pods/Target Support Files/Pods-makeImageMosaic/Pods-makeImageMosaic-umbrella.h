82e4fe475abb8eceafe09cfd4f437c63e715ef39
Victoria-Kravets <victoriakravets93@gmail.com>
2017-08-01 15:15:30 +0300

    - added swiftLint

---
+++ makeImageMosaic/Pods/Target Support Files/Pods-makeImageMosaic/Pods-makeImageMosaic-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_makeImageMosaicVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_makeImageMosaicVersionString[];
+
---
+++ makeImageMosaic/Pods/Target Support Files/Pods-makeImageMosaicTests/Pods-makeImageMosaicTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_makeImageMosaicTests : NSObject
+@end
+@implementation PodsDummy_Pods_makeImageMosaicTests
+@end
